facf75c70c0b33cc36a1f92c9839a7936b4b58a6
Alex <1321312087@qq.com>
2021-02-09 18:47:54 +0800

    类本身

---
+++ Phone/src/com/dome1/Phone.java
@@ -0,0 +1,19 @@
+package com.dome1;
+
+//import java.lang.System.Logger
+
+public class Phone{
+    String brand;
+    int price;
+    
+    public void call(String name){
+        
+        System.out.println(name);
+
+        //Logger.info("------")
+    }
+    public void sendMessage(String name){
+        System.out.println(name);
+    }
+    
+}
