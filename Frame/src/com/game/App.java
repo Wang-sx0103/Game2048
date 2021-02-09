0a9ea1bdea43a8bd14b0e57648658bbac309d393
Alex <1321312087@qq.com>
2021-02-09 19:14:07 +0800

    窗口文件

---
+++ Frame/src/com/game/App.java
@@ -0,0 +1,11 @@
+package com.game;
+
+import javax.swing.*;
+
+public class App{
+	public static void main(String[] args){
+		JFrame frame = new JFrame();
+		frame.setSize(500,600);
+		frame.setVisible(true);
+	}
+}
