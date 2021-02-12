50dddb6bb923e0171ef94fb5f64b93f5c94a8099
Alex <1321312087@qq.com>
2021-02-12 18:35:00 +0800

    完善Phone类功能

--- Phone/src/com/dome1/Phone.java
+++ Phone/src/com/dome1/Phone.java
@@ -1,16 +1,35 @@
 package com.dome1;
-
 //import java.lang.System.Logger
 
 public class Phone{
-    String brand;
-    int price;
-    
+    private String brand;
+    private int price;
+    //private String test
+    public Phone(){}
+    public Phone(String brand,int price){
+        this.brand = brand;
+        this.price = price;
+    }
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+    public void setPrice(int price){
+        this.price = price;
+    }
+    public String getBrand(){
+        return brand;
+    }
+    public int getPrice(){
+        return price;
+    }
+    public void showInfo(){
+        System.out.println("手机的价格是："+this.price);
+        System.out.println("手机的型号是："+this.brand);
+    }
     public void call(String name){
         
         System.out.println(name);
 
-        //Logger.info("------")
     }
     public void sendMessage(String name){
         System.out.println(name);
