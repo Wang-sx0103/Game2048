9e927df8d40812adf07b62a68a65cb5b1346a2d3
Alex <1321312087@qq.com>
2021-02-20 11:07:28 +0800

    增加菜单条，并加入主题选项

--- Frame/src/com/game1/MainFrame.java
+++ Frame/src/com/game1/MainFrame.java
@@ -3,98 +3,139 @@ package com.game1;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
 
-public class MainFrame extends JFrame implements KeyListener,ActionListener {
+public class MainFrame extends JFrame implements KeyListener, ActionListener, MenuListener {
     /**
      *
      */
     private static final long serialVersionUID = 1L;
-    //数组初始化
+    // 数组初始化
     private int[][] datas = new int[4][4];
-    //计分器
+    // 计分器
     private int score = 0;
+
+    // 图片资源的标识
+    private String theme = "A-";
+
+    // 创建菜单条下的选项
+    JMenuItem item1 = new JMenuItem("Classic");
+    JMenuItem item2 = new JMenuItem("Neon");
+    JMenuItem item3 = new JMenuItem("Candy");
 
     public MainFrame() {
         this.generateTwo();
         this.generateTwo();
+        this.initMenu();
         this.initFrame();
         this.paintView();
-        //为键盘添加监听
+        // 为键盘添加监听
         this.addKeyListener(this);
-        //窗口可见
-		super.setVisible(true);
+        // 窗口可见
+        super.setVisible(true);
+    }
+
+    /**
+     * 此方法用于初始化菜单条(换肤,关于我们)
+     */
+    public void initMenu() {
+        // 1. 创建菜单条
+        JMenuBar menuBar = new JMenuBar();
+        // 2. 创建栏目对象 JMenu (换肤, 关于我们)
+        JMenu menu1 = new JMenu("Theme");
+        JMenu menu2 = new JMenu("About");
+        // 3. 将菜单对象放到菜单条中
+        menuBar.add(menu1);
+        menuBar.add(menu2);
+        // 4. 将菜单项目放入栏目中
+        menu1.add(item1);
+        menu1.add(item2);
+        menu1.add(item3);
+        // 5. 创建菜单项目监听机制
+        item1.addActionListener(this);
+        item2.addActionListener(this);
+        item3.addActionListener(this);
+        menu2.addMenuListener(this);
+        // 6. 给窗体对象设置菜单条.
+        super.setJMenuBar(menuBar);
+
     }
 
     /**
      * 此方法用与初始化窗口，所有窗体的相关设置都在这个方法中完成
      */
-    public void initFrame(){
-		//设置窗口名称
-        super.setTitle("2048小游戏");
-        //设置窗口尺寸
-        super.setSize(514,538);
-		//窗口居中
-		super.setLocationRelativeTo(null);
-		//窗口置顶
-		super.setAlwaysOnTop(true);//窗体置顶
-		//退出窗口时关闭程序
-	  	super.setDefaultCloseOperation(3);
-	  	//取消默认布局
-		super.setLayout(null);
-
+    public void initFrame() {
+        // 设置窗口名称
+        super.setTitle("2048");
+        // 设置窗口尺寸
+        super.setSize(514, 538);
+        // 窗口居中
+        super.setLocationRelativeTo(null);
+        // 窗口置顶
+        super.setAlwaysOnTop(true);// 窗体置顶
+        // 退出窗口时关闭程序
+        super.setDefaultCloseOperation(3);
+        // 取消默认布局
+        super.setLayout(null);
     }
 
     /**
-     * 此方法用于绘制游戏界面 
+     * 此方法用于绘制游戏界面
      */
     public void paintView() {
         super.getContentPane().removeAll();
-        if(!check()) {
-            JLabel loseLabel = new JLabel(new ImageIcon("D:\\Desktop\\Demo\\Frame\\res\\image\\A-lose.png"));
+        if (!check()) {
+            JLabel loseLabel = new JLabel(new ImageIcon("D:\\Desktop\\Demo\\Frame\\res\\image\\" + theme + "lose.png"));
             loseLabel.setBounds(90, 100, 334, 228);
             super.getContentPane().add(loseLabel);
         }
-		for(int i = 0;i < 4;i++) {
-		  	for(int j = 0;j < 4;j++) {
-			    JLabel image = new JLabel(new ImageIcon("D:\\Desktop\\Demo\\Frame\\res\\image\\A-"+datas[i][j]+".png"));
-			    image.setBounds(50+100*j,50+100*i,100,100);
-			    super.getContentPane().add(image);
-			}
-		}
-		
-		JLabel background = new JLabel(new ImageIcon("D:\\Desktop\\Demo\\Frame\\res\\image\\A-Background.jpg"));
-        background.setBounds(40,40,420,420);
-        //获得一个容器，将控件放入其中
-	    super.getContentPane().add(background);
-        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                JLabel image = new JLabel(
+                        new ImageIcon("D:\\Desktop\\Demo\\Frame\\res\\image\\" + theme + datas[i][j] + ".png"));
+                image.setBounds(50 + 100 * j, 50 + 100 * i, 100, 100);
+                super.getContentPane().add(image);
+            }
+        }
+
+        JLabel background = new JLabel(
+                new ImageIcon("D:\\Desktop\\Demo\\Frame\\res\\image\\" + theme + "Background.jpg"));
+        background.setBounds(40, 40, 420, 420);
+        // 获得一个容器，将控件放入其中
+        super.getContentPane().add(background);
+
         JLabel scoreLabel = new JLabel("得分: " + this.score);
         scoreLabel.setBounds(50, 20, 100, 20);
         super.getContentPane().add(scoreLabel);
-        
-        //刷新界面
+
+        // 刷新界面
         super.getContentPane().repaint();
 
     }
-    
+
     /**
      * 在数列空白处随机产生2
      */
     private void generateTwo() {
-		// 1. 创建两个数组, 准备记录二维数组中空白格子 i 和 j 的索引位置.
-        //arrayI = {-1, -1, -1, -1, ...};
-        //arrayJ = {-1, -1, -1, -1, ...};
-        int[] arrayI = new int[datas.length*datas.length];
-        int[] arrayJ = new int[datas.length*datas.length];
-        for(int i = 0; i < arrayI.length; i++){
+        // 1. 创建两个数组, 准备记录二维数组中空白格子 i 和 j 的索引位置.
+        // arrayI = {-1, -1, -1, -1, ...};
+        // arrayJ = {-1, -1, -1, -1, ...};
+        int[] arrayI = new int[datas.length * datas.length];
+        int[] arrayJ = new int[datas.length * datas.length];
+        for (int i = 0; i < arrayI.length; i++) {
             arrayI[i] = -1;
             arrayJ[i] = -1;
         }
-        
+
         int k = 0;
 
         // 2. 遍历二维数组, 取出每一个元素, 并判断当前元素是否是空白格式 (判断是否是0)
@@ -117,13 +158,26 @@ public class MainFrame extends JFrame implements KeyListener,ActionListener {
             int y = arrayJ[index];
             datas[x][y] = 2;
         }
-	}
+    }
 
     @Override
-    public void actionPerformed(ActionEvent e) {}
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == item1) {
+            theme = "A-";
+        } else if (e.getSource() == item2) {
+            theme = "B-";
+        } else if (e.getSource() == item3) {
+            theme = "C-";
+        } else {
+            System.out.println("敬请期待");
+        }
+
+        paintView();
+    }
 
     @Override
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
@@ -131,30 +185,28 @@ public class MainFrame extends JFrame implements KeyListener,ActionListener {
 
         if (keyCode == 37) {
             // 调用左移动的方法.
-            moveToLeft();
-            generateTwo();
+            this.moveToLeft();
         } else if (keyCode == 38) {
             // 上移动
-            moveToTop();
-            generateTwo();
+            this.moveToTop();
         } else if (keyCode == 39) {
             // 调用右移动的方法
-            moveToRight();
-            generateTwo();
+            this.moveToRight();
         } else if (keyCode == 40) {
             // 调用下移动的方法
-            moveToBottom();
-            generateTwo();
+            this.moveToBottom();
         }
-
-        // 每一次移动的逻辑执行完, 都要去调用check方法, 检查游戏是否是失败的状态.
+        // 每一次移动完成,检查游戏是否
         this.check();
-        // 每一次移动完成, 重新绘制界面
+        // 每一次移动完成,检查数组能否生成2
+        this.generateTwo();
+        // 每一次移动完成,重新绘制界面
         this.paintView();
     }
-    
+
     @Override
-    public void keyReleased(KeyEvent e) {}
+    public void keyReleased(KeyEvent e) {
+    }
 
     private void moveToBottom() {
         // 1. 数组顺时针旋转
@@ -168,9 +220,9 @@ public class MainFrame extends JFrame implements KeyListener,ActionListener {
     private void moveToRight() {
         // 1. 反转数组
         this.horizontalSwap();
-         // 2. 左移动
+        // 2. 左移动
         this.moveToLeft();
-         // 3. 反转数组
+        // 3. 反转数组
         this.horizontalSwap();
 
     }
@@ -183,7 +235,7 @@ public class MainFrame extends JFrame implements KeyListener,ActionListener {
         // 3. 数组顺时针旋转
         this.clockwise();
     }
-    
+
     /**
      * 左移数组
      */
@@ -206,10 +258,10 @@ public class MainFrame extends JFrame implements KeyListener,ActionListener {
                 if (datas[i][x] == datas[i][x + 1]) {
                     datas[i][x] *= 2;
 
-                    // 计算得分
+                    // 1. 计算得分
                     this.score += datas[i][x];
 
-                    // 后续元素前移, 并在末尾补0
+                    // 2. 后续元素前移, 并在末尾补0
                     for (int j = x + 1; j < datas[i].length - 1; j++) {
                         datas[i][j] = datas[i][j + 1];
                     }
@@ -217,7 +269,7 @@ public class MainFrame extends JFrame implements KeyListener,ActionListener {
                     datas[i][datas[i].length - 1] = 0;
                 }
             }
-        }  
+        }
     }
 
     /**
@@ -230,6 +282,7 @@ public class MainFrame extends JFrame implements KeyListener,ActionListener {
             arr[end] = temp;
         }
     }
+
     /**
      * 用于处理二维数组的反转
      */
@@ -248,7 +301,7 @@ public class MainFrame extends JFrame implements KeyListener,ActionListener {
 
         for (int i = 0; i < datas.length; i++) {
             for (int j = 0; j < datas.length; j++) {
-                newArr[j][datas.length - (i+1)] = datas[i][j];
+                newArr[j][datas.length - (i + 1)] = datas[i][j];
             }
         }
 
@@ -263,7 +316,7 @@ public class MainFrame extends JFrame implements KeyListener,ActionListener {
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                newArr[datas.length - (j+1)][i] = datas[i][j];
+                newArr[datas.length - (j + 1)][i] = datas[i][j];
             }
         }
 
@@ -272,11 +325,11 @@ public class MainFrame extends JFrame implements KeyListener,ActionListener {
 
     /**
      * 判断整个数组能否上下左右移动
-     * @return
+     * 
+     * @return false:不能继续移动;true:可以继续移动
      */
     private boolean check() {
         if (checkLeft() == false && checkRight() == false && checkTop() == false && checkBottom() == false) {
-            // 失败的状态.
             return false;
         }
         return true;
@@ -286,20 +339,20 @@ public class MainFrame extends JFrame implements KeyListener,ActionListener {
      * 此方法判断是否可以左移动
      */
     private boolean checkLeft() {
-        // 1. 创建新数组, 用于备份原数组数据.
+        // 1. 创建新数组, 用于备份原数组数据
         int[][] newArr = new int[datas.length][datas.length];
-        // 2. 将原数组数据, 拷贝到新数组中.
+        // 2. 将原数组数据, 拷贝到新数组中
         copyArray(datas, newArr);
         // 3. 调用左移动方法, 对原数组数据进行左移动
         moveToLeft();
-        // 4. 使用移动后的数据, 和备份的数据逐个进行比对, 并使用flag变量进行记录.
-        // true : 可以移动，false : 不可以移动.
+        // 4. 使用移动后的数据, 和备份的数据逐个进行比对, 并使用flag变量进行记录
+        // true : 可以移动，false : 不可以移动
         boolean flag = false;
-        //比较两数组是否相同
+        // 比较两数组是否相同
         flag = this.compare(datas, newArr);
-        // 5. 确定信息后, 恢复原数组数据(再做一次拷贝)
-        copyArray(newArr, datas);
-        // 6. 返回结果信息.
+        // 5. 确定信息后, 恢复原数组数据(即再进行一次拷贝)
+        this.copyArray(newArr, datas);
+        // 6. 返回结果信息
         return flag;
     }
 
@@ -342,26 +395,28 @@ public class MainFrame extends JFrame implements KeyListener,ActionListener {
         return flag;
     }
 
-     /**
+    /**
      * 此方法用于二维数组的数据拷贝
+     * 
      * @param src  原数组
      * @param dest 目标数组
      */
-    private void copyArray(int[][] src, int[][] dest) {
-        for (int i = 0; i < src.length; i++) {
-            for (int j = 0; j < src[i].length; j++) {
-                dest[i][j] = src[i][j];
+    private void copyArray(int[][] srcArr, int[][] destArr) {
+        for (int i = 0; i < srcArr.length; i++) {
+            for (int j = 0; j < srcArr[i].length; j++) {
+                destArr[i][j] = srcArr[i][j];
             }
         }
     }
-    
+
     /**
      * 此方法用于比较两数组是否相同
+     * 
      * @param datas1 数组1
      * @param datas2 数组2
      * @return 不同：true,相同：false
      */
-    private boolean compare(int[][] datas1,int[][] datas2) {
+    private boolean compare(int[][] datas1, int[][] datas2) {
         boolean flag = false;
         for (int i = 0; i < datas1.length; i++) {
             for (int j = 0; j < datas1[i].length; j++) {
@@ -373,5 +428,17 @@ public class MainFrame extends JFrame implements KeyListener,ActionListener {
         }
         return flag;
     }
+
+    @Override
+    public void menuSelected(MenuEvent e) {
+        System.out.println("敬请期待");
+
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent e) {}
+
+    @Override
+    public void menuCanceled(MenuEvent e) {}
 
 }
